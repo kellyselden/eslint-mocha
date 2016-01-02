8e4e55cfa26992229ab4e162badbec7067324226
Kelly Selden <kellyselden@gmail.com>
2016-01-01 19:59:48 -0500

    tests

---
+++ test/acceptance/a-test.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+var exec = require('child_process').exec;
+
+describe('eslint-mocha', function() {
+  it('works', function(done) {
+    exec('node index.js', function(error, stdout, stderr) {
+      console.log(error);
+      console.log(stdout);
+      console.log(stderr);
+      done();
+    });
+  });
+});
