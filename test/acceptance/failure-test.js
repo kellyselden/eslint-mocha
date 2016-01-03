9f771f80a51df6f7a5228d35847e16fc19b92a31
Kelly Selden <kellyselden@gmail.com>
2016-01-03 17:47:41 -0500

    line numbers

--- test/acceptance/failure-test.js
+++ test/acceptance/failure-test.js
@@ -20,15 +20,15 @@ describe('acceptance - failure', function() {
       expect(stdout).to.contain('1 passing');
       expect(stdout).to.contain('2 failing');
       expect(stdout).to.contain(eol.auto('\n\
-        Expected indentation of 4 space characters but found 2.'));
+        Line 2: Expected indentation of 4 space characters but found 2.'));
       expect(stdout).to.contain('\n\
       -1\n\
       +0');
       expect(stdout).to.contain(eol.auto('\n\
-        "describe" is not defined.\n\
-        Expected indentation of 4 space characters but found 2.\n\
-        "it" is not defined.\n\
-        Expected indentation of 6 space characters but found 4.'));
+        Line 4: "describe" is not defined.\n\
+        Line 5: Expected indentation of 4 space characters but found 2.\n\
+        Line 5: "it" is not defined.\n\
+        Line 6: Expected indentation of 6 space characters but found 4.'));
       expect(stdout).to.contain('\n\
       -4\n\
       +0');
