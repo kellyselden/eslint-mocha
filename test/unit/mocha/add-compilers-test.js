d7d266374503d9262d659f187e2ebb2b3c9b2f0c
Kelly Selden <kellyselden@gmail.com>
2016-01-09 14:40:51 -0500

    add mocker

--- test/unit/mocha/add-compilers-test.js
+++ test/unit/mocha/add-compilers-test.js
@@ -1,22 +1,45 @@
 import { expect } from 'chai';
 import addCompilers from '../../../lib/mocha/add-compilers';
 
-describe('unit - addCompilers', function() {
-  it('pushes to extentsions', function() {
-    let compilers = ['js:babel-register'];
+describe('unit - mocha/addCompilers', function() {
+  beforeEach(function() {
+    addCompilers.__Rewire__('_require', () => {});
+  });
+
+  afterEach(function() {
+    addCompilers.__ResetDependency__('_require');
+  });
+
+  it('pushes to extensions', function() {
+    let compilers = ['test-ext:'];
     let extensions = [];
 
     addCompilers(compilers, extensions);
 
-    expect(extensions).to.deep.equal(['js']);
+    expect(extensions).to.deep.equal(['test-ext']);
   });
 
   it('does not duplicate extensions', function() {
-    let compilers = ['js:babel-register'];
-    let extensions = ['js'];
+    let compilers = ['test-ext:'];
+    let extensions = ['test-ext'];
 
     addCompilers(compilers, extensions);
 
-    expect(extensions).to.deep.equal(['js']);
+    expect(extensions).to.deep.equal(['test-ext']);
+  });
+
+  it('calls require with module name', function() {
+    let compilers = [':test-mod'];
+    let extensions = [];
+
+    let wasCalled;
+    addCompilers.__Rewire__('_require', module => {
+      expect(module).to.equal('test-mod');
+      wasCalled = true;
+    });
+
+    addCompilers(compilers, extensions);
+
+    expect(wasCalled).to.be.true;
   });
 });
